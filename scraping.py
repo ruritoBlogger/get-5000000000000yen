38728d6c6789ed727e4e2b05b783d39a7c7fd1ac
ruritoblogger <rurito.blog@gmail.com>
2019-10-09 05:26:42 +0900

    add scraping code

---
+++ scraping.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+from datetime import datetime
+import time
+
+#持ってくるナンバーズ3の過去のデータ
+#first_dataが1であれば1回目から取ってくる
+#last_dataが5020であれば5020回目まで取ってくる
+first_number = 1
+last_number = 5280
+
+#一回辺りに取ってくる量
+amount = 19
+#保存先のディレクトリ
+output = open('numbers.txt', mode='w')
+
+while first_number < last_number:
+        #スクレイピング先のurl
+    url = 'https://takarakuji.rakuten.co.jp/backnumber/numbers3_detail/{}-{}/'.format('{0:04d}'.format(first_number), '{0:04d}'.format(first_number + amount) )
+    soup = BeautifulSoup(requests.get(url).content,'html.parser')
+
+    tag_tr = soup.find_all('tr')
+    #テーブルの各データの取得
+    data = []
+    for i in range(1,len(tag_tr)):
+        data.append([d.text for d in tag_tr[i].find_all('td')])
+        cnt = 0;
+        for j in data[i-1]:
+            #日付だけ取得しない
+            if( cnt <= 1):
+                cnt += 1
+                continue
+            output.write(j)
+            output.write('\n')
+            cnt += 1
+    first_number += amount
+    time.sleep(1)

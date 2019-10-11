17405f92ea779bf557d05358f4ff6714178f314c
ruritoblogger <rurito.blog@gmail.com>
2019-10-11 21:16:42 +0900

    fix accuracy func

--- learning.py
+++ learning.py
@@ -23,7 +23,6 @@ def data_read( file_name, key):
     f = open( file_name, mode = "r" )
     f_string = f.readlines()
     data = np.array([] )
-    print("test")
     
     for i in range( 0, len( f_string ) ):
         #引数を用いて正解ラベルを振り分ける
@@ -81,10 +80,13 @@ class RNN(Chain):
         super(RNN, self).__init__()
         with self.init_scope():
             self.l1=L.LSTM(None, n_hidden).to_gpu()
-            self.l2=L.Linear(None, n_output).to_gpu()
+            self.l2=L.LSTM(None, n_hidden).to_gpu()
+            self.l3=L.Linear(None, n_hidden).to_gpu()
+            self.l4=L.Linear(None, n_output).to_gpu()
         
     def reset_state(self):
         self.l1.reset_state()
+        self.l2.reset_state()
         
     def __call__(self, x, t):
         y = self.predict(x)
@@ -98,20 +100,26 @@ class RNN(Chain):
     def accuracy(self, y, t):
         correct = 0
         for j in range( 0, len(y) ):
+            tmp = 0
             if y[j].size:
-                for i in range( 0, 3 ):
+                for i in range( 0, 4 ):
                     if( t[j][y[j].argmax()] ):
-                        correct += 1
+                        tmp += 1
                     y[j][y[j].argmax()]= np.amin(y[j])
-        return correct / (len(y) * 3)
+                if( tmp == 3 ):
+                    correct += 1
+        return correct / len(y)
         
     
     def predict(self, x):
         if train:
             h1 = F.dropout(self.l1(x),ratio = 0.5)
+            h2 = F.dropout(self.l2(h1),ratio = 0.5)
         else:
             h1 = self.l1(x)
-        return self.l2(h1)
+            h2 = self.l2(h1)
+        h3 = self.l3(h2)
+        return self.l4(h3)
 
 
 # In[69]:
@@ -146,7 +154,7 @@ class LSTMUpdater(training.StandardUpdater):
 data = list(zip(teachers, answers))
 N = len(data)
 n_batchsize = 30
-n_epoch = 1
+n_epoch = 1000
 
 #モデルを使う準備。オブジェクトを生成
 n_hidden = 10
@@ -162,40 +170,9 @@ test_iter = chainer.iterators.SerialIterator(test, n_batchsize, repeat=False, sh
 updater = LSTMUpdater(train_iter, optimizer, device=0)
 trainer = training.Trainer(updater, (n_epoch, "epoch"), out="result")
 trainer.extend(extensions.Evaluator(test_iter, model, device=0))
-trainer.extend(extensions.LogReport())
 trainer.extend(extensions.LogReport(trigger=(10, "epoch")))
 trainer.extend(extensions.PrintReport( ["epoch", "main/loss", "validation/main/loss", "main/accuracy", "validation/main/accuracy", "elapsed_time"])) # エポック、学習損失、テスト損失、学習正解率、テスト正解率、経過時間
 trainer.extend(extensions.PlotReport(['main/loss', 'val/main/loss'], x_key='epoch', file_name='loss.png'))
 trainer.extend(extensions.PlotReport(['main/accuracy', 'val/main/accuracy'], x_key='epoch', file_name='accuracy.png'))
 trainer.run()
-
-
-# In[27]:
-
-def accuracy(y, t):
-        correct = 0
-        for j in range( 0, len(y) ):
-            if y[j].size:
-                for i in range( 0, 3 ):
-                    if( t[j][y[j].argmax()] ):
-                        correct += 1
-                    y[j][y[j].argmax()]= np.amin(y[j])
-        print(correct)
-        return correct / (len(y) * 3)
-
-y = np.array([[ 2.32572779e-02, 1.50612950e-01, -7.73513615e-02,
-            1.24777101e-01, 1.66107967e-01, 4.45912518e-02,
-            3.39158834e-03, -3.49103138e-02, 6.63672294e-03,
-           -9.04915929e-02],
-            [ 2.32572779e-02, 1.50612950e-01, -7.73513615e-02,
-            1.24777101e-01, 1.66107967e-01, 4.45912518e-02,
-            3.39158834e-03, -3.49103138e-02, 6.63672294e-03,
-           -9.04915929e-02]])
-t = np.array([[0,1,0,1,1,0,0,0,0,0],[0,1,0,1,1,0,0,0,0,0]])
-accuracy(y,t)
-
-
-# In[ ]:
-
-
 

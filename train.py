bed027a3893fcf2d1aa3839549b508d3468981a4
rurito <rurito.blog@gmail.com>
2020-03-10 20:29:54 +0900

    fixup! fixup! 学習に用いるハイパーパラメーターを調整する部分を実装

--- train.py
+++ train.py
@@ -8,16 +8,31 @@ import os
 
 def main():
 
+    #バッチサイズ
     batch_size = 50
+
+    #学習回数
     epoch = 30
+
+    #過去何回分のデータを用いて次の回の当選番号を予想するか
     vertical_len = 3
 
+    #slackに通知を飛ばすかどうか
     useSlack = False
+
+    #既に学習させたモデルを使用するかどうか
     is_load_other_model = False
 
+    #誤差の遷移図の名前
     loss_picture_name = "loss.png"
+    
+    #精度の遷移図の名前
     accuracy_picture_name = "acc.png"
+
+    #保存するモデルの名前
     cnn_model_name = "model.npz"
+
+    #既に学習させたモデルを用いる場合のモデルのパス
     use_cnn_model_name = "model.npz"
 
     observer = CNN_observer(vertical_len)

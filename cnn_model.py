6be86fbf1590045baab46e3c451263942a397476
rurito <rurito.blog@gmail.com>
2020-03-10 18:51:54 +0900

    CNNの入出力層を定義

--- cnn_model.py
+++ cnn_model.py
@@ -5,24 +5,25 @@ import chainer.functions as F
 import chainer.links as L
 import numpy as np
 
-
 class CNN(Chain):
 
     def __init__(self):
         super(CNN,self).__init__(
-            conv0 = L.Convolution2D(),
-            conv1 = L.Convolution2D(),
-            conv2 = L.Convolution2D(),
-            conv3 = L.Convolution2D(),
-            affine1 = L.Linear(),
-            affine2 = L.Linear()
+            conv0 = L.Convolution2D(1, 2, (1,2), pad=1),
+            conv1 = L.Convolution2D(2, 4, (1,2), pad=1),
+            conv2 = L.Convolution2D(4, 8, (1,2), pad=1),
+            conv3 = L.Convolution2D(8, 16, (1,2), pad=1),
+            affine1 = L.Linear(None, 1000),
+            affine2 = L.Linear(None, 10)
         )
 
     def forward(self, x):
+        x = np.array(x).astype(np.float32)
+        x = x.reshape([int(x.size/x[0].size), 1, int(x[0].size/10), 10])
         h0 = F.max_pooling_2d(F.relu(self.conv0(x)), 2)
-        h1 = F.max_pooling_2d(F.relu(self.conv0(h0)), 2)
-        h2 = F.max_pooling_2d(F.relu(self.conv0(h1)), 2)
-        h3 = F.max_pooling_2d(F.relu(self.conv0(h2)), 2)
+        h1 = F.max_pooling_2d(F.relu(self.conv1(h0)), 2)
+        h2 = F.max_pooling_2d(F.relu(self.conv2(h1)), 2)
+        h3 = F.max_pooling_2d(F.relu(self.conv3(h2)), 2)
 
         h4 = self.affine1(h3)
         h5 = self.affine2(h4)
@@ -30,6 +31,8 @@ class CNN(Chain):
         return F.softmax(h5)
 
     def debug_forward(self, x):
+        x = np.array(x).astype(np.float32)
+        x = x.reshape([int(x.size/x[0].size), 1, int(x[0].size/10), 10])
         self.make_picture("before.png",x)
         
         h0 = F.max_pooling_2d(F.relu(self.conv0(x)), 2)

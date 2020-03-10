7412ecba19b9078fc098ed1ea444b0540101516e
rurito <rurito.blog@gmail.com>
2020-03-10 18:43:54 +0900

    fixup! 学習に用いるハイパーパラメーターを調整する部分を実装

--- train.py
+++ train.py
@@ -1,31 +1,36 @@
 from cnn_updater import CNN_updater
 from cnn_observer import CNN_observer
+from trainer import Trainer
 
 from slackbot import SlackBot
 from dotenv import load_dotenv
-from os.path import join
+import os
 
 def main():
 
     batch_size = 50
     epoch = 30
+    vertical_len = 3
 
     useSlack = False
+    is_load_other_model = False
 
     loss_picture_name = "loss.png"
     accuracy_picture_name = "acc.png"
     cnn_model_name = "model.npz"
+    use_cnn_model_name = "model.npz"
 
-    observer = CNN_Observer()
+    observer = CNN_observer(vertical_len)
     updater = CNN_updater()
+    updater.initialize(is_load_other_model, use_cnn_model_name)
 
     # dotenv用の初期設定 + slackAPIの初期設定
-    dotenv_path = join(dirname(__file__), '.env')
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
     load_dotenv(dotenv_path)
     token = os.environ.get("API_KEY")
     slack = SlackBot(token)
 
-    trainer = CNNTrainer(batch_size, epoch, slack, observer, updater, useSlack, loss_picture_name, accuracy_picture_name, cnn_model_name)
+    trainer = Trainer(batch_size, epoch, slack, observer, updater, useSlack, loss_picture_name, accuracy_picture_name, cnn_model_name)
 
     trainer.learn()
 

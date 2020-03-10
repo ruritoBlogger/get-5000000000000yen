21fe8d7664b5f407de9c2b7e699a73866395da70
rurito <rurito.blog@gmail.com>
2020-03-10 16:03:40 +0900

    学習の中心部分を実装

---
+++ trainer.py
@@ -0,0 +1,88 @@
+class Trainer():
+
+    def __init__(self, batch_size, epoch, slack, observer, model, useSlack, loss_picture_name, acc_picture_name):
+        self.batch_size = batch_size
+        self.epoch = epoch
+        self.useSlack = useSlack
+        self.loss_picture_name = loss_picture_name
+        self.acc_picture_name = acc_picture_name
+
+        self._slack = slack
+        self._observer = observer
+        self._model = model
+
+        self.loss_list = []
+        self.acc_list = []
+
+
+    def learn(self):
+        data = self.getData("./numbers.txt")
+        teach, ans = self.observer.transform(data)
+
+        train_teach = teach[0:len(teach)*0.8]
+        train_ans = ans[0:len(ans)*0.8]
+
+        test_teach = teach[len(teach)*0.8:]
+        test_ans = ans[len(ans)*0.8:]
+
+        for j in range( self.epoch ):
+
+            for i in range( int(len(train_teach) / self.batch_size ) ):
+                batch_teach = train_teach[i: i+self.batch_size]
+                batch_ans = train_ans[i: i+self.batch_size]
+
+                if( len(batch_teach) == 0 ):
+                    break
+
+                self.model.update(batch_teach, batch_ans)
+            
+            loss, accuracy = self.model.evaluate(test_teach, test_ans)
+
+            self.loss_list.append(loss)
+            self.acc_list.append(accuracy)
+            self.send_message(j)
+
+        self.make_picture()
+        self.save_model()
+    
+    def send_message(self, episode):
+        message = ""
+        message += "現在{0}エピソード\n".format(episode)
+        if(self.useSlack): 
+            self.slack.send_message(message, "#random")
+
+    def make_picture(self):
+        plt.figure()
+        plt.plot(self.loss_list, label="loss")
+        plt.savefig(self.loss_picture_name)
+
+        plt.figure()
+        plt.plot(self.acc_list, label="acc")
+        plt.savefig(self.acc_picture_name)
+
+    def save_model(self):
+        self.model.save()
+
+    @property
+    def slack(self):
+        return self._slack
+
+    @slack.setter
+    def slack(self, slack):
+        print("please don't overwrite slack instance")
+
+    @property
+    def observer(self):
+        return self._observer
+
+    @observer.setter
+    def observer(self, observer):
+        print("please don't overwrite observer instance")
+
+    @property
+    def model(self):
+        return self._model
+
+    @model.setter
+    def model(self, model):
+        print("please don't overwrite model instance")
